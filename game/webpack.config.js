a00d5f23748a59d043caaff53baf3c538ed8833e
Martin <sileshs@gmail.com>
2019-03-08 14:06:21 +0000

    adding folder structure

---
+++ game/webpack.config.js
@@ -0,0 +1,10 @@
+const config = {
+  entry: `${__dirname}/src/app.js`,
+  output: {
+    path: `${__dirname}/public/js`,
+    filename: 'bundle.js'
+  },
+  mode: 'development' -> update to development once complete.
+};
+
+module.exports = config;
