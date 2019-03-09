c3a9a9b137e4bc0209b3a4e139eb9d07a63529f1
Martin <sileshs@gmail.com>
2019-03-09 08:22:56 +0000

    Creating initial server file

---
+++ game/client/src/model/data_load.js
@@ -0,0 +1,14 @@
+const DataLoad = function() {
+  this.images = [
+  {id: 1, value: 1, url:"images/1.jpg"},
+  {id: 2, value: 2, url:"images/2.jpg"},
+  {id: 3, value: 3, url:"images/3.jpg"},
+  {id: 4, value: 1, url:"images/1.jpg"},
+  {id: 5, value: 2, url:"images/2.jpg"},
+  {id: 6, value: 3, url:"images/3.jpg"}
+]
+}
+
+DataLoad.prototype.duplicate = function () {
+
+};
