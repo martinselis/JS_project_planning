9a3debfb3eb4ac2f0f78e983d2c3c84e9c833896
dvioletta <Anita@Pixie.local>
2019-03-09 18:19:58 +0000

    fixing typos

--- game/client/src/views/square_view.js
+++ game/client/src/views/square_view.js
@@ -1,5 +1,5 @@
 const PubSub = require('../helpers/pub_sub');
-const MasterView = require('./views/master_view');
+const MasterView = require('./views/master_view')
 
 const SquareView = function (container) {
   this.container = container
@@ -8,7 +8,7 @@ const SquareView = function (container) {
 //render all cards to grid
 
 //on event listen for flip cards
-const cards = document.querySelectorAll(.memory-card);
+const cards = document.querySelectorAll('.memory-card')
 
 function flipCard() {
   this.classList.toggle('flip');
