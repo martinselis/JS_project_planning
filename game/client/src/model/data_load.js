89f1c44719b8949d48b1b6f0a2eddb1ed96d921b
Martin <sileshs@gmail.com>
2019-03-09 09:12:33 +0000

    adding data loader files and test files

--- game/client/src/model/data_load.js
+++ game/client/src/model/data_load.js
@@ -1,14 +1,21 @@
+const PubSub = require('../helpers/pub_sub.js')
+
 const DataLoad = function() {
+
   this.images = [
-  {id: 1, value: 1, url:"images/1.jpg"},
-  {id: 2, value: 2, url:"images/2.jpg"},
-  {id: 3, value: 3, url:"images/3.jpg"},
-  {id: 4, value: 1, url:"images/1.jpg"},
-  {id: 5, value: 2, url:"images/2.jpg"},
-  {id: 6, value: 3, url:"images/3.jpg"}
-]
+    {id: 1, value: 1, url:"images/1.jpeg"},
+    {id: 2, value: 2, url:"images/2.jpeg"},
+    {id: 3, value: 3, url:"images/3.jpg"},
+    {id: 4, value: 4, url:"images/4.png"},
+    {id: 5, value: 5, url:"images/5.jpg"},
+    {id: 6, value: 6, url:"images/6.jpeg"}
+    {id: 7, value: 7, url:"images/7.png"},
+    {id: 8, value: 8, url:"images/8.jpeg"}
+  ]
 }
 
-DataLoad.prototype.duplicate = function () {
-
+DataLoad.prototype.publishData = function () {
+  PubSub.publish('DataLoad:images-ready', this.images)
 };
+
+module.exports = DataLoad;
