a0ec5812f51d1b5beb462c15e2afc67c0eb507af
dvioletta <anita@anitasquires.com>
2019-03-10 16:58:50 +0000

    adding more functions to square_view

--- game/client/src/views/square_view.js
+++ game/client/src/views/square_view.js
@@ -1,11 +1,32 @@
 const PubSub = require('../helpers/pub_sub');
-const MasterView = require('./views/master_view')
+// const MasterView = require('./views/master_view')
 
 const SquareView = function (container) {
+  this.classList = classList;
   this.container = container
+  this.card = card
 }
 
 //render all cards to grid
+SquareView.prototype.bindEvents = function(){
+  this.container = evt.detail;
+  this.render()
+}
+
+SquareView.prototype.render = function(){
+  const container = document.createElement('memory-game');
+  this.container.innerHTML = '';
+  this.container.appendChild(container);
+
+  const image = this.createCardImage()
+  container.appendChild(image)
+}
+
+SquareView.prototype.createCardImage = function() {
+  const image = document.createElement('img');
+
+  return image
+}
 
 //on event listen for flip cards
 const cards = document.querySelectorAll('.memory-card')
