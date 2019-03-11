38ae7548375c95603f7e9dc54fc49c0a6a929553
dvioletta <anita@anitasquires.com>
2019-03-11 10:42:14 +0000

    Added shuffler

--- game/client/src/model/data_load.js
+++ game/client/src/model/data_load.js
@@ -1,5 +1,6 @@
 const PubSub = require('../helpers/pub_sub.js')
 const images = require('./images.js')
+const shuffle_function = require('./shuffle_function.js')
 const DataLoad = function() {
 
 }
@@ -45,26 +46,8 @@ DataLoad.prototype.setImageCount = function (setImageCount) {
     this.images = requiredImages;
     this.images.push(copys)
     this.images = this.images.flat();
+    this.images = shuffle_function(this.images);
 };
 
+
 module.exports = DataLoad;
-
-
-// this.images = [
-//   {id: 1, value: 1, url:"images/1.jpeg"},
-//   {id: 2, value: 2, url:"images/2.jpeg"},
-//   {id: 3, value: 3, url:"images/3.jpg"},
-//   {id: 4, value: 4, url:"images/4.png"},
-//   {id: 5, value: 5, url:"images/5.jpg"},
-//   {id: 6, value: 6, url:"images/6.jpeg"},
-//   {id: 7, value: 7, url:"images/7.png"},
-//   {id: 8, value: 8, url:"images/8.jpeg"},
-//   {id: 9, value: 1, url:"images/1.jpeg"},
-//   {id: 10, value: 2, url:"images/2.jpeg"},
-//   {id: 11, value: 3, url:"images/3.jpg"},
-//   {id: 12, value: 4, url:"images/4.png"},
-//   {id: 13, value: 5, url:"images/5.jpg"},
-//   {id: 14, value: 6, url:"images/6.jpeg"},
-//   {id: 15, value: 7, url:"images/7.png"},
-//   {id: 16, value: 8, url:"images/8.jpeg"}
-// ]
