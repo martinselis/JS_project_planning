d4243431b07ea97b3794be0b632046a237805950
Martin <sileshs@gmail.com>
2019-03-12 10:59:54 +0000

    adding leadearboard functionality

--- game/server/db/seed_data.js
+++ game/server/db/seed_data.js
@@ -1,35 +1,15 @@
 use scoreboard;
 db.dropDatabase();
 
-<<<<<<< HEAD
 db.entries.insertMany([
-{name: 'Steve',
-clicks: 40,
-time:100
-},
+  {name: 'Steve',
+  clicks: 40,
+  time:100
+  },
 
-{name: "Cathy",
-clicks: 60,
-time: 130
-}
+  {name: "Cathy",
+  clicks: 60,
+  time: 130
+  }
 
 ])
-=======
-db.entry.insertMany([
-  {
-    name: 'Steve',
-    clicks: 45,
-    time: 100
-  },
-  {
-    name: 'Cathy'
-    clicks: 64,
-    time: 250
-  },
-  {
-    name: 'Andy'
-    clicks: 30,
-    time: 45
-  },
-]);
->>>>>>> f398a7ee6046e1ad99b3d14ebfd915c4d36b3e4f
